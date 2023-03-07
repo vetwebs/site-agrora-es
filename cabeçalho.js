ace8f5da644cd06ae573ab9ee3371a6a9653483f
Ana Rita Queiroz <anaritadequeiroz@hotmail.com>
2023-03-07 15:00:50 -0300

    finalizando cabeçalho

---
+++ cabeçalho.js
@@ -0,0 +1,10 @@
+const menuDiv = document.getElementById('menu-mobile')
+
+const btnAnimar = document.getElementById('btn-menu')
+
+menuDiv.addEventListener('click', animar)
+
+function animar(){
+    menuDiv.classList.toggle('abrir')
+    btnAnimar.classList.toggle('ativo')
+}
